e494892cc220df6e8011c9a390711a33ebed4f7c
Abdul-karim164 <abdul.karim3@students.mq.edu.au>
2026-06-01 08:54:59 +1000

    Java HTTP server from Week 1

---
+++ MyHandler.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class MyHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+        String origin = he.getRequestHeaders().getFirst("Origin");
+        if (origin != null) {
+            he.getResponseHeaders().add("Access-Control-Allow-Origin", origin);
+            he.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            he.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+        }
+        Headers headers = he.getResponseHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Connection", "close");
+
+        String response = "{\"name\":\"Australia\",\"gold\":18,\"silver\":19,\"bronze\":16}";
+        he.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = he.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
